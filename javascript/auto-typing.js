00f52f9dd8a565e53e5d89f3d6c83896b57114b2
borinmorm21 <borinmorm5@gmail.com>
2024-05-14 17:59:19 +0700

    first commit

---
+++ javascript/auto-typing.js
@@ -0,0 +1,7 @@
+//auto-type
+var typed = new Typed(".auto-type", {
+            strings: ["Frontend Developer", "Web Developer", "Freelancer", "Mobile Developer"],
+            typeSpeed: 40,
+            backSpeed: 20,
+            loop: true
+        })
